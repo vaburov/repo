ec6351f0dc81365c9e87f4cccd05316ef51d2b16
vaburov <vaburov@gmail.com>
2015-06-27 01:00:31 +0300

    Sample

---
+++ buddhainthecity/buddhainthecity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buddhainthecity
+//
+//  Created by Валентин Буров on 27.06.15.
+//  Copyright (c) 2015 Valentin Burov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
